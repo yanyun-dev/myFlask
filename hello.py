c7cf83ac61942bc7dbf8b52b0c8646879ceb367e
yanyun <yanyun@outlook.com>
2018-01-28 14:19:55 +0800

    3a test

--- hello.py
+++ hello.py
@@ -1,11 +1,25 @@
 #! /bin/env/python
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
+
 @app.route('/')
 def index():
-	return '<h1>Hello,World!</h1>'
+	return render_template('index.html')
 
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name=name)
+
+@app.errorhandler(404):
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+@app.errorhandler(500):
+def internal_server_error(e):
+	return render_template('500.html'), 500
 if __name__=='__main__':
-	app.run()
+	bootstrap.run()
