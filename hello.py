c5292dfc13193eca1ee43a2b126ba42efce7fe39
yanyun <yanyun@outlook.com>
2018-01-28 19:57:46 +0800

    cdn changed

--- hello.py
+++ hello.py
@@ -3,15 +3,28 @@ from datetime import datetime
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+	name = StringField('What is your name?', validators=[DataRequired()])
+	submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html', current_time=datetime.utcnow())
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
