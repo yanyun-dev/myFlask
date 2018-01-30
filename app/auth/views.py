83431bf7e073ce8eb8a56e67e64d121233a0b5c5
yanyun <yanyun@outlook.com>
2018-01-30 21:45:59 +0800

    login

---
+++ app/auth/views.py
@@ -0,0 +1,28 @@
+from flask import render_template, redirect, url_for, flash
+from . import auth
+from flask_login import login_required
+from ..models import User
+from .forms import LoginForm
+
+@auth.route('/secret')
+@login_required
+def secret():
+    return 'Only authenticated users are allowed!'
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
+
+@auth.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password.')
+    return render_template('auth/login.html')
--- app/models.py
+++ app/models.py
@@ -1,5 +1,11 @@
 from . import db
 from werkzeug import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
 
 class Role(db.Model):
 	__tablename__ = 'roles'
@@ -10,11 +16,13 @@ class Role(db.Model):
 		return '<Role %r>' % self.name
 	users = db.relationship('User', backref='role')
 
-class User(db.Model):
+class User(UserMixin, db.Model):
 	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(64), unique=True, index=True)
 	username = db.Column(db.String(64), unique=True, index=True)
 	password_hash = db.Column(db.String(128))
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 	@property
 	def password(self):
