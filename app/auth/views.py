d0da5e0e7009eb230fdbd645f9a06c32affa45cd
yanyun <yanyun@outlook.com>
2018-02-01 20:08:00 +0800

    can register

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, redirect, url_for, flash, request
 from . import auth
 from flask_login import login_required, login_user
-from ..models import User
-from .forms import LoginForm
+from ..models import User, db
+from .forms import LoginForm, RegistrationForm
 
 @auth.route('/secret')
 @login_required
@@ -26,3 +26,15 @@ def login():
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username = form.username.data,
+                    password = form.password.data)
+        db.session.add(user)
+        flash('You can now login')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
